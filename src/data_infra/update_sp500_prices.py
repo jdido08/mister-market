06a92a5fee8155670f3d19846cf33f6a17b11c63
jdido08 <jdido08@gmail.com>
2021-10-24 15:13:09 -0400

    testing

--- src/data_infra/update_sp500_prices.py
+++ src/data_infra/update_sp500_prices.py
@@ -24,10 +24,10 @@ def get_sp500_prices():
 
     return df
 
-print(sys.path[0])
-print(os.path.abspath(os.getcwd()))
-print(os.getcwd())
-print(os.path.dirname(os.path.abspath(__file__)))
+# print(sys.path[0])
+# print(os.path.abspath(os.getcwd()))
+# print(os.getcwd())
+# print(os.path.dirname(os.path.abspath(__file__)))
 
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -38,36 +38,36 @@ db_hostname = cfg["mysql"]["DB_HOSTNAME"]
 db_port = cfg["mysql"]["DB_PORT"]
 db_name = cfg["mysql"]["RAW_DATA_DB_NAME"]
 
-db_ssl_ca_path = os.path.abspath(os.getcwd()) + '/ssl/server-ca.pem'
-db_ssl_cert_path = os.path.abspath(os.getcwd()) + '/ssl/client-cert.pem'
-db_ssl_key_path = os.path.abspath(os.getcwd()) + '/ssl/client-key.pem'\
+db_ssl_ca_path = os.path.dirname(os.path.abspath(__file__)) + '/ssl/server-ca.pem'
+db_ssl_cert_path = os.path.dirname(os.path.abspath(__file__)) + '/ssl/client-cert.pem'
+db_ssl_key_path = os.path.dirname(os.path.abspath(__file__)) + '/ssl/client-key.pem'\
 
 # print(db_ssl_ca_path)
 # print(db_ssl_cert_path)
 # print(db_ssl_key_path)
-#
-# #https://towardsdatascience.com/sql-on-the-cloud-with-python-c08a30807661
-# engine = sqlalchemy.create_engine(
-#      sqlalchemy.engine.url.URL.create(
-#         drivername="mysql+pymysql",
-#         username=db_user,  # e.g. "my-database-user"
-#         password=db_pass,  # e.g. "my-database-password"
-#         host=db_hostname,  # e.g. "127.0.0.1"
-#         port=db_port,  # e.g. 3306
-#         database=db_name,  # e.g. "my-database-name"
-#     ),
-#     connect_args = {
-#         'ssl_ca': db_ssl_ca_path ,
-#         'ssl_cert': db_ssl_cert_path,
-#         'ssl_key': db_ssl_key_path
-#     }
-# )
-#
-#
-# try:
-#     sp500_price = get_sp500_prices()
-#     print('get sp500 sucesss')
-#     sp500_price.to_sql('sp500_prices', engine, if_exists='replace', index=False, chunksize=500)
-#     print("SUCCESS: sp500_prices updated")
-# except Exception as e:
-#     print("Can't update sp500_prices -- Error: ", e)
+
+#https://towardsdatascience.com/sql-on-the-cloud-with-python-c08a30807661
+engine = sqlalchemy.create_engine(
+     sqlalchemy.engine.url.URL.create(
+        drivername="mysql+pymysql",
+        username=db_user,  # e.g. "my-database-user"
+        password=db_pass,  # e.g. "my-database-password"
+        host=db_hostname,  # e.g. "127.0.0.1"
+        port=db_port,  # e.g. 3306
+        database=db_name,  # e.g. "my-database-name"
+    ),
+    connect_args = {
+        'ssl_ca': db_ssl_ca_path ,
+        'ssl_cert': db_ssl_cert_path,
+        'ssl_key': db_ssl_key_path
+    }
+)
+
+
+try:
+    sp500_price = get_sp500_prices()
+    print('get sp500 sucesss')
+    sp500_price.to_sql('sp500_prices', engine, if_exists='replace', index=False, chunksize=500)
+    print("SUCCESS: sp500_prices updated")
+except Exception as e:
+    print("Can't update sp500_prices -- Error: ", e)
